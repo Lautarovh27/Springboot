5316827492806d4ba9cefa60d7f524265afc1296
Lautaro <lautarovanh@gmail.com>
2026-04-30 21:56:37 -0300

    Se agrego lista de usuarios con thymeleaf

--- src/main/java/com/lautaro/curso/springboot/webapp/springboot_web/controllers/UserController.java
+++ src/main/java/com/lautaro/curso/springboot/webapp/springboot_web/controllers/UserController.java
@@ -1,10 +1,16 @@
 package com.lautaro.curso.springboot.webapp.springboot_web.controllers;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import com.lautaro.curso.springboot.webapp.springboot_web.controllers.models.User;
+import org.springframework.web.bind.annotation.RequestParam;
+
 
 
 @Controller
@@ -19,5 +25,16 @@ public class UserController {
         model.addAttribute("user",user);
         return "details";
     }
+
+    @GetMapping("/list")
+    public String list(ModelMap model) {
+        List<User> users = new ArrayList<>();
+
+        model.addAttribute("users", users);
+        model.addAttribute("title", "Listado de usuarios");
+        return "list";
+        
+    }
+    
 
 }
