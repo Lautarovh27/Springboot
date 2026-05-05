e2ab1e55ff5cf1e9a94bf03c51380523a67109a1
Lautaro <lautarovanh@gmail.com>
2026-05-05 11:54:56 -0300

    Se creo un ParamDto para recibir mensajes en JSON, nuevo RequestParamsController y ParamDto class

---
+++ src/main/java/com/lautaro/curso/springboot/webapp/springboot_web/controllers/RequestParamsController.java
@@ -0,0 +1,24 @@
+package com.lautaro.curso.springboot.webapp.springboot_web.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.lautaro.curso.springboot.webapp.springboot_web.controllers.models.dto.ParamDto;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
+@RestController
+@RequestMapping("/api/params")
+public class RequestParamsController {
+
+    @GetMapping("/foo")
+    public ParamDto foo (@RequestParam(required = false, defaultValue = "No se ha recibido ningun mensaje") String message) {
+        ParamDto param = new ParamDto();
+        param.setMessage(message);
+        return param;
+    }
+    
+
+}
