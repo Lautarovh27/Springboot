eb418775b79c494956b0b257adc724e9247a8853
Lautaro <lautarovanh@gmail.com>
2026-05-05 15:32:34 -0300

    Se comenzo a utilizar postman en vez de navegador

--- src/main/java/com/lautaro/curso/springboot/webapp/springboot_web/controllers/PathVariableController.java
+++ src/main/java/com/lautaro/curso/springboot/webapp/springboot_web/controllers/PathVariableController.java
@@ -3,6 +3,7 @@ package com.lautaro.curso.springboot.webapp.springboot_web.controllers;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.lautaro.curso.springboot.webapp.springboot_web.controllers.models.User;
 import com.lautaro.curso.springboot.webapp.springboot_web.controllers.models.dto.ParamDto;
 
 import java.util.HashMap;
@@ -10,6 +11,9 @@ import java.util.Map;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
 
 
 
@@ -33,6 +37,14 @@ public class PathVariableController {
         json.put("id", id);
         return json;
     }
+    
+    @PostMapping("/create")
+    public User create(@RequestBody User user){
+        user.setName(user.getName().toUpperCase());
+        return user;
+
+    }
+    
     
     
 }
